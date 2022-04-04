630b349b32628b194896ec1bdb68ab3bcd5b4835
Sagar Mochi <sagarmochi@TECH-SAGAR.local>
2022-04-04 17:06:49 +0530

    added meta description

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
     <>
       <Head>
         <html lang='en' />
+        <meta name="description" content="Meta description content goes here." />
         <title>test next.js</title>
       </Head>
       <Navbar />
